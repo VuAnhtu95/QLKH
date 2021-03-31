f75aa4af6d31ce10dd229bebebf77427832c1ff4
VuAnhtu95 <75408266+VuAnhtu95@users.noreply.github.com>
2021-03-31 16:25:49 +0700

    add

---
+++ src/main/java/Model/Service/ICustomerService.java
@@ -0,0 +1,6 @@
+package Model.Service;
+
+import Model.Entities.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
---
+++ src/main/java/Repository/ICustomerRepository.java
@@ -0,0 +1,7 @@
+package Repository;
+
+import Model.Entities.Customer;
+
+public interface ICustomerRepository extends IGeneralRepository<Customer>{
+
+}
