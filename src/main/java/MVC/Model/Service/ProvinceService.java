92b5889a0f061e506795f3a4bfa8122444399fe3
VuAnhtu95 <75408266+VuAnhtu95@users.noreply.github.com>
2021-04-01 13:44:03 +0700

    add

--- src/main/java/MVC/Model/Service/ProvinceService.java
+++ src/main/java/MVC/Model/Service/ProvinceService.java
@@ -4,28 +4,30 @@ import MVC.Model.Entities.Province;
 import MVC.Repository.IProvinceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
 public class ProvinceService implements IProvinceService{
 
     @Autowired
     private IProvinceRepository provinceRepository;
 
     @Override
-    public Iterable<Province> findAll() {
-        return null;
+    public List<Province> findAll() {
+        return (List<Province>) provinceRepository.findAll();
     }
 
     @Override
     public Province findById(Long id) {
-        return null;
+        return provinceRepository.findById(id).get();
     }
 
     @Override
     public void save(Province province) {
-
+        provinceRepository.save(province);
     }
 
     @Override
     public void remove(Long id) {
-
+        provinceRepository.deleteById(id);
     }
 }
